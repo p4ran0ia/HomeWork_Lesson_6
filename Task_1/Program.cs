a71b67963190a61febcdfa59268698999531adbc
Alexandr <crazy.top@mail.ru>
2022-09-17 11:12:30 +0300

    complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿int M = new Random().Next(1, 10);
+int count = 0;
+Console.WriteLine("Input numbers: ");
+for (int i = 1; i <= M; i++)
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0) count++; 
+}
+Console.WriteLine($"Numbers > 0: {count}");
---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input coordinates: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+Console.WriteLine((k1 * x + b1) + " " + (k2 * x + b2));
